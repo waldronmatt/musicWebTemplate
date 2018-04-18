396b668054759ecd6aac9df93b2cbd3f38bf323d
Abel Chen <cettoana@gmail.com>
2018-04-18 11:57:52 +0800

    Fix sticky navigation bug on firefox

--- js/fixedNav.js
+++ js/fixedNav.js
@@ -2,10 +2,14 @@ var  mn = $(".container");
     mns = "container-scrolled";
     hdr = $('header').height();
 
-$(window).scroll(function() {
+function checkContainerScrolled() {
   if( $(this).scrollTop() > hdr ) {
     mn.addClass(mns);
   } else {
     mn.removeClass(mns);
   }
-});
+}
+
+$(window).scroll(checkContainerScrolled);
+
+$(window).ready(checkContainerScrolled);
