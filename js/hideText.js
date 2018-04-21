47b202807703d27cb2a6680f5b476c45586b0910
waldronmatt <waldronmatt@github.com>
2018-04-21 14:25:09 -0400

    Read bio button animation
    Added show and hide button animation to the "read bio" button on the "about" section.

--- js/hideText.js
+++ js/hideText.js
@@ -1,8 +1,10 @@
 		function myMusic() {
 			var x = document.getElementById("about-text");
 			if (x.className === "hide") {
+				$('.hide').hide().fadeIn(750);
 				x.className += "show";
 			} else {
 				x.className = "hide";
+				$('.hide').show().fadeOut(1000);
 			}
 		}
