d0144425c4d4875dd9a7c48810abadb9f1452c83
waldronmatt <waldronmatt@github.com>
2018-05-04 20:13:09 -0400

    Email validation & error modal
    Added email validation to the mialing list form.
    
    Added a second modal that displays when an invalid email is entered.
    
    Removed unused HTML id's and added CSS classes.

--- js/mailingList.js
+++ js/mailingList.js
@@ -1,14 +1,24 @@
 var $form = $('form#email-form'),
     url = 'https://script.google.com/macros/s/AKfycbz0p3AaSvvG2muFU1jLUQmaRBK3aSRxcF-_gcGrnIweDdfZc8KS/exec'
 
-$('#submit-form').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-  );
+$("input[type$='submit']").on('click', function(e) {
+    var x = document.forms["email-form"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+		e.preventDefault();
+		modalError();
+    }
+	else {
+		modal();
+		  e.preventDefault();
+		  var jqxhr = $.ajax({
+			url: url,
+			method: "GET",
+			dataType: "json",
+			data: $form.serializeObject()
+		  }).success(
+			// do something
+		  );
+		}
 })
